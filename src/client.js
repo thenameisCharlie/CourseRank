bb6537a8a7619dc833663002658d7633abd6fe7b
CarlosVillatoro <a7xcharlie18@gmail.com>
2024-02-28 02:33:10 -0500

    Removed variable

--- src/client.js
+++ src/client.js
@@ -2,6 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 const URL = import.meta.env.REACT_APP_SUPABASE_URL;
 const API_KEY = import.meta.env.REACT_APP_API_KEY;
-const damn = import.meta.env;
 
 export const supabase = createClient(URL, API_KEY);
